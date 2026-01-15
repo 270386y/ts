a6c835bbd1b73c685b58ce8155a367a9f2520314
270286v <270286v@ugs.kochi-tech.ac.jp>
2026-01-15 17:40:13 +0900

    選択したらページに行く

--- html/yuza-senntaku/yuza-senntaku.js
+++ html/yuza-senntaku/yuza-senntaku.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
     employeeBtn.addEventListener('click', function() {
         console.log("被雇用者が選ばれました");
         // ログイン画面へ移動
-        window.location.href = "main.html";
+        window.location.href = "../hikoyousya/roguin/hiroguin.html";
     });
 
     // 雇用者ボタン
     employerBtn.addEventListener('click', function() {
         console.log("雇用者が選ばれました");
         // ここも必要に応じて移動先を変えてください
-        window.location.href = "main.html";
+        window.location.href = "../koyou/koyou-roguin/koyou-roguin.html";
     });
 });
